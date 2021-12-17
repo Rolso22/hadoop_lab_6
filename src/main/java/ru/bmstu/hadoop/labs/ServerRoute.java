e2fa7abf50d401cc1a4c4e226a86cfd66b06a3b4
Rolso22 <ramagf@ynadex.ru>
2021-12-18 00:49:55 +0300

    Scripted auto-commit on change (2021-12-18 00:49:55) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/ServerRoute.java
+++ src/main/java/ru/bmstu/hadoop/labs/ServerRoute.java
@@ -48,6 +48,8 @@ public class ServerRoute {
 
     private CompletionStage<Response> sendToServer(String url, int count) {
         return Patterns.ask(storeActor, new GetServer(), Duration.ofMillis(TIME_OUT_MILLIS))
-                .thenCompose(answer -> sendRequest((String) answer));
-    } // TODO
+                .thenCompose(answer -> {
+
+                });
+    }
 }
