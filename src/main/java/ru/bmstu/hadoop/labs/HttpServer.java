7f46bcfc246e78394ecf3f456e0f456ab5072ca4
Rolso22 <ramagf@ynadex.ru>
2021-12-17 21:03:36 +0300

    Scripted auto-commit on change (2021-12-17 21:03:36) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/HttpServer.java
+++ src/main/java/ru/bmstu/hadoop/labs/HttpServer.java
@@ -1,15 +1,22 @@
 package ru.bmstu.hadoop.labs;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 import static ru.bmstu.hadoop.labs.Constants.*;
 
@@ -32,7 +39,13 @@ public class HttpServer implements Watcher {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         storeActor = system.actorOf(Props.create(StoreActor.class));
-        
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = route.createFlow();
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(DEFAULT_HOST, DEFAULT_PORT),
+                materializer
+        );
     }
 
     public void end() throws InterruptedException {
