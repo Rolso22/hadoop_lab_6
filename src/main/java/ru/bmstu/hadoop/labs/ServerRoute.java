7a6f9a6e913fc7ef50761863d1143229886be8a0
Rolso22 <ramagf@ynadex.ru>
2021-12-17 21:35:43 +0300

    Scripted auto-commit on change (2021-12-17 21:35:43) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/ServerRoute.java
+++ src/main/java/ru/bmstu/hadoop/labs/ServerRoute.java
@@ -6,10 +6,13 @@ import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.Route;
 import akka.stream.javadsl.Flow;
 
+import static akka.http.javadsl.server.Directives.route;
+
 public class ServerRoute {
 
 
     public Route createRoute() {
-        return Route
+        return route(
+                get())
     }
 }
