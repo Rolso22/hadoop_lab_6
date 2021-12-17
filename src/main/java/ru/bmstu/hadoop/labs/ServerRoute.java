e19d2ee97c2e58d557c0c7556b60c66300ded875
Rolso22 <ramagf@ynadex.ru>
2021-12-17 21:41:29 +0300

    Scripted auto-commit on change (2021-12-17 21:41:29) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/ServerRoute.java
+++ src/main/java/ru/bmstu/hadoop/labs/ServerRoute.java
@@ -14,6 +14,8 @@ public class ServerRoute {
                 get(() -> parameter(URL, (url) ->
                         parameter(COUNT, (count) -> {
                             
-                        }))))
+                        }))
+                )
+        );
     }
 }
