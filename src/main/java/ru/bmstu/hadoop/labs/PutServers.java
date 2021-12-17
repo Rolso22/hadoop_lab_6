c3972b22b33c522c4f97ed3a443c114f98533aa1
Rolso22 <ramagf@ynadex.ru>
2021-12-18 00:44:55 +0300

    Scripted auto-commit on change (2021-12-18 00:44:55) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/PutServers.java
+++ src/main/java/ru/bmstu/hadoop/labs/PutServers.java
@@ -1,4 +1,11 @@
 package ru.bmstu.hadoop.labs;
 
+import java.util.List;
+
 public class PutServers {
+    private List<String> serversList;
+
+    public PutServers(List<String> serversList) {
+        this.serversList = serversList;
+    }
 }
