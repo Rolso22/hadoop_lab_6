e733fb3466fd3f3979c75917b0c918cc93500cc3
Rolso22 <ramagf@ynadex.ru>
2021-12-17 14:36:43 +0300

    Scripted auto-commit on change (2021-12-17 14:36:43) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AnonymizerApp.java
+++ src/main/java/ru/bmstu/hadoop/labs/AnonymizerApp.java
@@ -2,6 +2,8 @@ package ru.bmstu.hadoop.labs;
 
 public class AnonymizerApp {
     public static void main(String[] args) {
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
         
     }
 }
