db278c88e904e479e9819d9d061acb33fe4e7803
Rolso22 <ramagf@ynadex.ru>
2021-12-17 22:04:50 +0300

    Scripted auto-commit on change (2021-12-17 22:04:50) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/ZooServer.java
+++ src/main/java/ru/bmstu/hadoop/labs/ZooServer.java
@@ -1,19 +1,25 @@
 package ru.bmstu.hadoop.labs;
 
+import akka.actor.ActorRef;
+import org.apache.zookeeper.ZooKeeper;
+
 public class ZooServer {
     private String path;
     private String host;
     private int port;
+    private ZooKeeper zoo;
+    private ActorRef storeActor;
 
-    public ZooServer(String path, String host, int port) {
+    public ZooServer(String path, String host, int port, ZooKeeper zoo, ActorRef storeActor) {
         this.path = path;
         this.host = host;
         this.port = port;
-        start();
+        this.zoo = zoo;
+        this.storeActor = storeActor;
     }
 
     private void start() {
-        
+
     }
 
 }
