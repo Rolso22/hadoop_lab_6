3d5639b9c726a8e2b7c55c128f4c8b43f90717d8
Rolso22 <ramagf@ynadex.ru>
2021-12-17 15:07:28 +0300

    Scripted auto-commit on change (2021-12-17 15:07:28) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/StoreActor.java
+++ src/main/java/ru/bmstu/hadoop/labs/StoreActor.java
@@ -1,4 +1,10 @@
 package ru.bmstu.hadoop.labs;
 
-public class StoreActor {
+import akka.actor.AbstractActor;
+
+public class StoreActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
