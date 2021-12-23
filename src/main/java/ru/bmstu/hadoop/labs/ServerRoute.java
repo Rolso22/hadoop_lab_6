e8369587f8c6ea6df0b51ee7a18d8b5d4f7ed80e
Rolso22 <ramagf@ynadex.ru>
2021-12-23 21:57:42 +0300

    Scripted auto-commit on change (2021-12-23 21:57:42) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/ServerRoute.java
+++ src/main/java/ru/bmstu/hadoop/labs/ServerRoute.java
@@ -45,7 +45,7 @@ public class ServerRoute {
     }
 
     private CompletionStage<Response> sendRequest(Request path) {
-        System.out.println("send request: " + path);
+        System.out.println("send request: " + path.toString());
         return httpClient.executeRequest(path).toCompletableFuture();
     }
 
