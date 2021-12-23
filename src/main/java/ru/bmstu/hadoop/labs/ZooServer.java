ebfdf504d9b459e4b84e4e08bacfc3fc2e41c0c1
Rolso22 <ramagf@ynadex.ru>
2021-12-23 21:13:42 +0300

    Scripted auto-commit on change (2021-12-23 21:13:42) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/ZooServer.java
+++ src/main/java/ru/bmstu/hadoop/labs/ZooServer.java
@@ -43,6 +43,7 @@ public class ZooServer implements Watcher {
         } catch (KeeperException | InterruptedException e) {
             e.printStackTrace();
         }
+        System.out.println("serversList: " + serversList);
         storeActor.tell(new PutServers(serversList), ActorRef.noSender());
 
     }
